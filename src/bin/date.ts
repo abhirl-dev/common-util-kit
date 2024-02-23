945d8ea7d1ff22efe07daf2cd84b40a3e258159f
abhirl <abimonrl@gmail.com>
2024-02-23 09:56:08 +0530

    initial release for npm

--- src/bin/date.ts
+++ src/bin/date.ts
@@ -2,10 +2,16 @@ import { DateConverterProps, DateReturnProps } from "./types.interface";
 
 class ConvertDate {
 
+    /**
+     * To convert date or timestamp to desired format.
+     */
     static fromTimestamp(props: DateConverterProps): string | number {
         const _dt: DateReturnProps = _buildDate(props.timeStamp);
 
         switch (props.format) {
+            case "milliSec":
+                return _dt.milliseconds;
+
             case "DD-MM-YYYY":
                 return `${_dt.dd}-${_dt.mm}-${_dt.yyyy}`;
 
@@ -18,8 +24,23 @@ class ConvertDate {
             case "Mon, DD-YYYY":
                 return `${_dt.month}, ${_dt.dd}-${_dt.yyyy}`;
 
+            case "DD Mon, YYYY":
+                return `${_dt.dd} ${_dt.month}, ${_dt.yyyy}`;
+
+            case "Mon DD, YYYY":
+                return `${_dt.month} ${_dt.dd}, ${_dt.yyyy}`;
+
+            case "YYYY, Mon DD":
+                return `${_dt.yyyy}, ${_dt.month} ${_dt.dd}`;
+
+            case "Mon DD YYYY":
+                return `${_dt.month} ${_dt.dd} ${_dt.yyyy}`;
+
+            case "Mon DD":
+                return `${_dt.month} ${_dt.dd}`;
+
             default:
-                return `${_dt}`;
+                return `${props.timeStamp}`;
         }
     }
 }
@@ -28,16 +49,16 @@ export { ConvertDate };
 
 const _monthArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-function _buildDate(dateTime: Date): DateReturnProps {
+function _buildDate(dateTime: string | number): DateReturnProps {
     const date: Date = new Date(dateTime);
 
     const dd = date.getDate().toString().padStart(2, "0").slice(-2);
     const mm = (date.getMonth() + 1).toString().padStart(2, "0").slice(-2);
     const yyyy = date.getFullYear().toString();
     const yy = date.getFullYear().toString().slice(-2);
-    const tt = date.getTime();
 
     return {
+        milliseconds: date.getMilliseconds(),
         dd: dd,
         mm: mm,
         yyyy: yyyy,
--- src/bin/types.interface.ts
+++ src/bin/types.interface.ts
@@ -1,14 +1,22 @@
 interface ColorProps {
-    hex: string,
-    alpha?: number,
+    r: number,
+    g: number,
+    b: number,
 }
 
 interface DateConverterProps {
-    timeStamp: any,
-    format: "DD-MM-YYYY" | "YYYY-MM-DD" | "DD-MM-YY" | "Mon, DD-YYYY" | "Mon DD, YYYY" | "YYYY, Mon DD" | "DD Mon, YYYY",
+    /**
+     * Timestamp should be either date string or milliseconds
+     * @example 
+     * const dateString = "1970-01-01"
+     * const milliSec = 1708659189784
+     */
+    timeStamp: string | number,
+    format: "milliSec" | "DD-MM-YYYY" | "YYYY-MM-DD" | "DD-MM-YY" | "Mon, DD-YYYY" | "Mon DD, YYYY" | "YYYY, Mon DD" | "DD Mon, YYYY" | "Mon DD YYYY" | "Mon DD",
 };
 
 interface DateReturnProps {
+    milliseconds: number;
     yyyy: string,
     yy: string,
     mm: string,
